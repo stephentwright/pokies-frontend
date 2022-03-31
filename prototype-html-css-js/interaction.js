13886fbb3378840eb560e3d7b26aca024ed262b9
Stephen Wright <st.wright@gmail.com>
2022-03-31 22:46:38 +1100

    added placehold web components

--- prototype-html-css-js/interaction.js
+++ prototype-html-css-js/interaction.js
@@ -1,17 +1,18 @@
 // environmental variables;
 const mapKey = "pk.eyJ1Ijoic3RlcGhlbnR3cmlnaHQiLCJhIjoiY2twODhhcTJlMDZqdjJvb2Y2ZTBxZzZzNiJ9.T-ETd1zEPRxEruIkdjy49w";
-const mapCentre = [-33.4488233,151.3786264];
-const mapZoom = 10;
+const mapCentre = [-33.32,151.3186264];
+const mapZoom = 11;
 
 const apiPath = '/prototype-html-css-js/resources/'
 const geoJSON = 'geoJSON/'
-const lgaCode = '18450'
+const lgaCode = '11650'
 
 // basic initalisation of the map;
 var map = L.map('map-container', { zoomControl: false} ).setView(mapCentre, mapZoom);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 16,
+    minZoom: 7,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
@@ -39,8 +40,10 @@ fetch(apiPath+premises+premisesLga)
     })
     .then((data) => {
         for (let i = 0; i < data.length; i++){
+            const clubInfo = `<h2>${data[i]["Licence name"]}</h2>
+                              <p>The number of electronic gaming machines (EGMs) is at the venue is currently <b>${data[i].EGMs}</b>   .</p>`;
             L.marker([data[i].Latitude, data[i].Longitude])
-             .bindPopup(data[i]["Licence name"])
+             .bindPopup(clubInfo)
              .addTo(map)
         }
     });
