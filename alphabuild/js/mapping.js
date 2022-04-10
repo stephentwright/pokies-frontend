25e45f4f7a371574b22a8b40ab2b9d3acb5167e8
Stephen Wright <st.wright@gmail.com>
2022-04-10 23:16:55 +1000

    add popup information -- needs more styling

--- alphabuild/js/mapping.js
+++ alphabuild/js/mapping.js
@@ -66,8 +66,45 @@ async function createLgaPopupInformation(lgaIds) {
                           <p>The total net profit for the period 1st Dec 2020 to 31 May 2021 was <b>$${lgaInfo["Net Profit"]}</b>. </p>
                           <p>There are <b>${lgaInfo["Premises Count"]}</b> clubs in the area with a total of <b>${lgaInfo.EGMs}</b> of electronic gaming machines (EGMs)</p>`;
 
+    //deffine the popup code injection
+    const customOptions = {
+        'className': 'popup-container'
+    }
+    const lgaPopupInfoCustom = `  
+                                <div class="rank-lga-name">
+                                    <div class="icon"><p>#1</p></div>
+                                    <div class="statistic"><p>${lgaInfo.lgaNameClean} LGA</p></div>
+                                </div>
+                                <div class="general-stats">
+                                    <div class="icon">
+                                        <img src="/components/popup-lga/resources/clubs-icon64.png" alt="Number of clubs">
+                                    </div>
+                                    <div class="statistic"><p>${lgaInfo["Premises Count"]} clubs or venues</p></div>
+                                    <div class="icon">
+                                        <img src="/components/popup-lga/resources/pokies-icon64.png" alt="Number of Pokie machines">
+                                    </div>
+                                    <div class="statistic"><p>${lgaInfo.EGMs} pokie machines</p></div>
+                                    <div class="icon">
+                                        <img src="/components/popup-lga/resources/money-fire-icon64.png" alt="Net profit">
+                                    </div>
+                                    <div class="statistic"><p>$${lgaInfo["Net Profit"]} net profit (6 mth)</p></div>
+                                    <div class="icon">
+                                        <img src="/components/popup-lga/resources/people-icon64.png" alt="Number of Adults">
+                                    </div>
+                                    <div class="statistic"><p>255,215 adults (18+ years)</p></div>
+                                    </div>
+                                <div class="featured-stat">
+                                    <div class="icon">
+                                        <img src="/components/popup-lga/resources/avo-toast-icon64.png" alt="Smashed Avo Index">
+                                    </div>
+                                    <div class="statistic">
+                                        <p>24 smashed avos (per adult)</p>
+                                    </div>
+                                </div>
+                            `;
+
     //add to the LGA-level popup information to the map                      
-    lgaLayer.bindPopup(lgaPopupInfo).addTo(map);
+    lgaLayer.bindPopup(lgaPopupInfoCustom,customOptions).addTo(map);
 }
 
 //load venue information with a brief popup message about the venue;
